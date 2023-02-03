bd49783fcc21bb9c486fe36ea2f17beebfe7fd18
ChathurangaPri <hachathurangapriyadarshani@gmail.com>
2023-02-04 02:37:34 +0530

    Second change

--- src/A.java
+++ src/A.java
@@ -4,6 +4,10 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("New Change 1");
 		System.out.println("New Change 2");
+
+
+		System.out.println("New Change 3");
+		System.out.println("New Change 4");
 	}
 
 }
